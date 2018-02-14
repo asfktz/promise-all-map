42906314333dc3195b95f85cdc4c025d8fa5a57e
Asaf Katz <asf.ktz@gmail.com>
2018-02-14 19:17:47 +0200

    iterate over objects

--- index.js
+++ index.js
@@ -3,12 +3,47 @@ function defaults (value, defaultValue) {
   return value === undefined ? defaultValue : value;
 }
 
-function identity (value) { return value; }
+function identity (value) {
+  return value;
+}
+
+function map (iterable, mapper) {
+  if (Array.isArray(iterable)) {
+    return iterable.map(mapper);
+  }
+
+  return Object.keys(iterable)
+    .reduce(function (obj, key, arr) {
+      obj[key] = mapper(iterable[key], key, i, arr);
+      return obj;
+    }, {});
+}
+
+function resolve (iterable) {
+    if (Array.isArray(iterable)) {
+      return Promise.all(iterable);
+    }
+
+    var keys = Object.keys(iterable);
+    var values = Object.values(iterable);
+
+    return Promise.all(values)
+      .then(function (resolvedVals)  {
+        return resolvedVals.reduce((obj, val, i) => {
+          obj[keys[i]] = val;
+          return obj;
+        }, {});
+      });
+}
 
 function all(iterable, mapper) {
   iterable = defaults(iterable, []);
   mapper = defaults(mapper, identity);
-  return Promise.all(iterable.map(mapper));
+
+  return resolve(iterable)
+    .then(function (iterable) {
+      return resolve(map(iterable, mapper))
+    });
 }
 
 module.exports = all;
